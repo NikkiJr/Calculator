682b5a1ab4db509db3ad2c286781674665f9c942
NikkiJr <161018738+NikkiJr@users.noreply.github.com>
2024-07-16 20:04:56 +0200

    Code

--- Code.py
+++ Code.py
@@ -17,8 +17,6 @@ elif op == "/":
     print(num1 / num2)
 elif op == "*":
     print(num1 * num2)
-elif op == "**":
-    print(num1 ** num2)
 else: #print('Invaild Operater')
     print('Invaild Operater')
 #print('This statement is always executed')
